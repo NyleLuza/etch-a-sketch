87ad7097b2c271240a368f753af4ea974f840900
NyleLuza <Jluza21@gmail.com>
2023-06-21 11:51:58 -0700

    created files

---
+++ web.js
@@ -0,0 +1,10 @@
+const container = document.getElementsByClassName('.rightColumn');
+const sizes = document.querySelector('#size');
+let boxSize;
+sizes.addEventListener('click',()=>{
+    boxSize=5;
+});
+
+function grid(boxSize){
+    
+}
